47123a8aae568201c1137a6b256911fe18f314d3
MohammedAlawna <88289155+MohammedAlawna@users.noreply.github.com>
2022-07-16 23:57:58 +0100

    usingContext

--- 01-starting-project/src/store/cart-context.js
+++ 01-starting-project/src/store/cart-context.js
@@ -1,9 +1,10 @@
 import React from "react";
-React.createContext({
+
+const CartContext = React.createContext({
   items: [],
   totalAmount: 0,
-  addItem: () => {},
-  removeItem: () => {},
+  addItem: (item) => {},
+  removeItem: (id) => {},
 });
 
 export default CartContext;
